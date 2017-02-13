46c5c29ccf139a59a14aca1969157e5bb2bd8b64
akarolak <artur.karolak@primaris.eu>
2017-02-13 14:57:37 +0100

    Issue: wtf

---
+++ readme.txt
@@ -0,0 +1,3 @@
+git push -u origin master
+
+sudo cp ../vagrant/SpringApp-1.0-SNAPSHOT.war var/lib/tomcat7/webapps
--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -19,6 +19,6 @@ sudo apt-get install -y software-properties-common python-software-properties
 echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update
-sudo apt-get install oracle-java7-installer
+sudo apt-get install oracle-java8-installer
 echo "Setting environment variables for Java 7.."
-sudo apt-get install -y oracle-java7-set-default
+sudo apt-get install -y oracle-java8-set-default
